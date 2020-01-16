aa45e243cbcf0c8b0ed7cda8469914179e47007a
chib31 <chib31@outlook.com>
2020-01-16 21:28:10 +0000

    removed global cors stuff

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -18,22 +18,4 @@ public class ServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class, args);
-  }
-
-  // Fix the CORS errors
-  @Bean
-  public FilterRegistrationBean simpleCorsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    // *** URL below needs to match the Vue client URL and port ***
-    config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
-    config.setAllowedMethods(Collections.singletonList("*"));
-    config.setAllowedHeaders(Collections.singletonList("*"));
-    source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    return bean;
-  }
-
-}
+  }}
