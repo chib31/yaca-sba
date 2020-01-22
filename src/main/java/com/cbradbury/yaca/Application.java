230574a62ca37bc54c07c205ff0f8a4d0b4201ce
chib31 <chib31@outlook.com>
2020-01-22 23:54:36 +0000

    project config

--- src/main/java/com/cbradbury/yaca/Application.java
+++ src/main/java/com/cbradbury/yaca/Application.java
@@ -2,7 +2,9 @@ package com.cbradbury.yaca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value="application.properties")
 @SpringBootApplication
 public class Application {
 
